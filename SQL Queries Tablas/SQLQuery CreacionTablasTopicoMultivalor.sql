b30ef1b763404a1f89c1f2c82fcdc38d99e71a87
Marco Mora <54404955+MarcoMora85@users.noreply.github.com>
2020-04-30 13:19:50 -0600

    Posibles queries para la creación de tablas necesarias para 1ra iteración
    La carpeta tiene dos queries distintos: 
    CreacionTablasTopicoMultivalor.sql donde la tabla de Tópico depende de la de Artículo, así es como se plantea tópico como un atributo multivaluado de Artículo.
    CreacionTablasTopicoEntidad.sql donde la tabla de Tópico representa una entidad independiente, no un atributo multivaluado. La relación que hay entre los tópicos y los artículos se representa con la tabla TopicosArticulos.

---
+++ SQL Queries Tablas/SQLQuery CreacionTablasTopicoMultivalor.sql
@@ -0,0 +1,54 @@
+CREATE TABLE Autor
+(	usernamePK NVARCHAR(20) NOT NULL,
+	email	NVARCHAR(50) NOT NULL,
+	nombre	NVARCHAR(30) NOT NULL,
+	apellido1 NVARCHAR(30) NOT NULL,
+	apellido2	NVARCHAR(30) NOT NULL,
+	pais NVARCHAR(30),
+	estado NVARCHAR(30),
+	ciudad	NVARCHAR(30),
+	imagenPerfil NVARCHAR(MAX) NOT NULL,
+	hobbies NVARCHAR(MAX),
+	habilidades NVARCHAR(MAX),
+	idiomas NVARCHAR(MAX)
+
+	CONSTRAINT PK_Autor PRIMARY KEY (usernamePK),
+	CONSTRAINT UQ_Autor_email UNIQUE (email)
+);
+
+CREATE TABLE Articulo
+(
+	idArticuloPK INTEGER IDENTITY(0,1) NOT NULL,
+	titulo NVARCHAR(200) NOT NULL,
+	resumen NVARCHAR(MAX) NOT NULL,
+	tipo INTEGER NOT NULL,
+	contenido NVARCHAR(MAX) NOT NULL,
+	fechaPublicacion DATE NOT NULL
+
+	CONSTRAINT PK_Articulo PRIMARY KEY (idArticuloPK),
+	CONSTRAINT UQ_Articulo_titulo UNIQUE (titulo),
+	CONSTRAINT CK_Articulo_tipo CHECK (tipo IN (0,1))
+);
+
+CREATE TABLE Topico
+(
+	nombreTopico NVARCHAR(50),
+	idArticuloFK INTEGER
+
+	CONSTRAINT PK_Topico PRIMARY KEY (nombreTopico, idArticuloFK),
+	CONSTRAINT FK_Topico_Articulo FOREIGN KEY (idArticuloFK) REFERENCES Articulo(idArticuloPK)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
+
+CREATE TABLE ImagenArticuloCorto
+(
+	idImagenPK INTEGER IDENTITY(0,1),
+	rutaImagen NVARCHAR(MAX) NOT NULL,
+	idArticuloFK INTEGER NOT NULL
+
+	CONSTRAINT PK_ImagenArticuloCorto PRIMARY KEY (idImagenPK),
+	CONSTRAINT FK_ImagenArticuloCorto_Articulo FOREIGN KEY (idArticuloFK) REFERENCES Articulo(idArticuloPK)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
