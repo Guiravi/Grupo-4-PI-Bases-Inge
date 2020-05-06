82da48ae601da1c14216def80c89cd8e3f0fbef4
Marco Mora <marco8mora@gmail.com>
2020-05-05 20:20:50 -0600

    Controlador de Articulo con Guardar Sobrecargado

--- TheCoffeePlace/TheCoffeePlace/EscribirArticulo.aspx.cs
+++ TheCoffeePlace/TheCoffeePlace/EscribirArticulo.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 using TheCoffeePlace.Views;
 using TheCoffeePlace.Controllers;
 
-public partial class EscribirArticulo : System.Web.UI.Page, IView_EscribirArticulo, IView_SubirArticulo, IView_ImagenArticulo
+public partial class EscribirArticulo : System.Web.UI.Page, IView_EscribirArticulo, IView_ImagenArticulo
 {
 
     protected void Page_Load(object sender, EventArgs e)
@@ -28,17 +28,11 @@ public partial class EscribirArticulo : System.Web.UI.Page, IView_EscribirArticu
 		set { txtResumen.Text = value; }
 	}
 
-	string IView_EscribirArticulo.contenido
+	string contenido
 	{
 		get { return ftxtEditor.Text; }
 		set { ftxtEditor.Text = value; }
 	}
-
-    
-    byte[] IView_SubirArticulo.contenido
-    {
-        get { return new byte[0]; }
-    }
 
     public int tipo
 	{
