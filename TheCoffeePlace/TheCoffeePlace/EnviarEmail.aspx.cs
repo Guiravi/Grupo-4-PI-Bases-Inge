593bef3bf4b3380dc5f5c30e1e5858040838557f
KewQiu <7310376+KewQiu@users.noreply.github.com>
2020-05-09 03:04:36 -0600

    Interfaz Grafica para Enviar Email Finalizado

---
+++ TheCoffeePlace/TheCoffeePlace/EnviarEmail.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class EnviarEmail : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBoxDestino_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBoxAsunto_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBoxMensaje_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void ResetButton_Click(object sender, EventArgs e)
+    {
+        TextBoxMensaje.Text = string.Empty;
+    }
+
+    protected void SendButton_Click(object sender, EventArgs e)
+    {
+
+    }
+}
