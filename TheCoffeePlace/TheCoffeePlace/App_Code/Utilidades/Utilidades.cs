39f4efebc44fc2ff927f2064bfcbf4cf0d3c40f9
Silvio Salazar <silviosalac@gmail.com>
2020-05-10 15:31:47 -0600

    Se pueden convertir archivos .docx a .pdf utilizando el archivo soffice.exe(Se incluye todo libreoffice por las dependencias).

--- TheCoffeePlace/TheCoffeePlace/App_Code/Utilidades/Utilidades.cs
+++ TheCoffeePlace/TheCoffeePlace/App_Code/Utilidades/Utilidades.cs
@@ -6,6 +6,8 @@ using System.Web;
 using System.Net;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Diagnostics;
+using System.Reflection;
 
 /// <summary>
 /// Summary description for Utilidades
@@ -56,8 +58,11 @@ namespace TheCoffeePlace.Utilities
 			byte[] fileBuffer = User.DownloadData(filePath);
 			if (fileBuffer != null)
 			{
+				string[] splitString = pathVirtualArchivoPDF.Split('/');
+				string fileName = splitString[splitString.Length - 1];
 				pagina.Response.ContentType = "application/pdf";
 				pagina.Response.AddHeader("content-length", fileBuffer.Length.ToString());
+				pagina.Response.AppendHeader("Content-Disposition", "filename=" + fileName);
 				pagina.Response.BinaryWrite(fileBuffer);
 			}
 		}
@@ -68,6 +73,34 @@ namespace TheCoffeePlace.Utilities
 			page.Response.Redirect(redirectVirtualPath);
 		}
 
+
+		public class LibreOfficeFailedException : Exception
+		{
+			public LibreOfficeFailedException(int exitCode)
+				: base(string.Format("LibreOffice has failed with {0}", exitCode))
+			{ }
+		}
+
+		static public void DocxToPDF(string docxVirtualPath)
+		{
+			string libreOfficePath = HttpContext.Current.Server.MapPath("~/libreoffice/program/soffice.exe");
+			string docxFullPath = HttpContext.Current.Server.MapPath(docxVirtualPath);
+			string pdfsFullPath = HttpContext.Current.Server.MapPath("~/ArticulosPDF");
+			string stringArgs = "--convert-to pdf " + docxFullPath + " --outdir " + pdfsFullPath;
+			ProcessStartInfo procStartInfo = new ProcessStartInfo(libreOfficePath, stringArgs);
+			procStartInfo.RedirectStandardOutput = true;
+			procStartInfo.UseShellExecute = false;
+			procStartInfo.CreateNoWindow = true;
+			procStartInfo.WorkingDirectory = Environment.CurrentDirectory;
+			Process process = new Process() { StartInfo = procStartInfo, };
+			process.Start();
+			process.WaitForExit();
+
+			if (process.ExitCode != 0)
+			{
+				throw new LibreOfficeFailedException(process.ExitCode);
+			}
+		}
     }
 }
 
--- TheCoffeePlace/TheCoffeePlace/SubirArticulo.aspx.cs
+++ TheCoffeePlace/TheCoffeePlace/SubirArticulo.aspx.cs
@@ -55,7 +55,13 @@ public partial class SubirArticulo : System.Web.UI.Page, IView_SubirArticulo
 		{
 			TopicoController topicoController = new TopicoController();
 			topicoController.SetTopicos(this);
-
+			List<string> listaAutores = new List<string>();
+			listaAutores.Add("Nombre del autor");
+			ViewState["listaAutores"] = listaAutores;
+			gvAutor.DataSource = listaAutores;
+			gvAutor.DataBind();
+			Utilidades.DocxToPDF("~/ArticulosDOCX/Agile.docx");
+			Utilidades.VerPDF(this, "~/ArticulosPDF/Agile.pdf");
 		}
 	}
 
@@ -74,4 +80,49 @@ public partial class SubirArticulo : System.Web.UI.Page, IView_SubirArticulo
         }
     }
 
+	protected void gvAutor_RowEditing(object sender, GridViewEditEventArgs e)
+	{
+		gvAutor.EditIndex = e.NewEditIndex;
+		gvAutor.DataSource = (List<string>)ViewState["listaAutores"];
+		gvAutor.DataBind();
+	}
+
+
+	protected void gvAutor_RowUpdated(object sender, GridViewUpdateEventArgs e)
+	{
+	}
+
+	protected void gvAutor_RowUpdating(object sender, GridViewUpdateEventArgs e)
+	{
+		List<string> listaAutores = (List<string>)ViewState["listaAutores"];
+		listaAutores[e.RowIndex] = (gvAutor.Rows[e.RowIndex].Cells[1].Controls[0] as TextBox).Text;
+		gvAutor.DataSource = (List<string>)ViewState["listaAutores"];
+		gvAutor.EditIndex = -1;
+		gvAutor.DataBind();
+	}
+
+	protected void btnAgregarAutor_Click(object sender, EventArgs e)
+	{
+		List<string> listaAutores = (List<string>)ViewState["listaAutores"];
+		listaAutores.Add(txtNuevoAutor.Text);
+		gvAutor.DataSource = (List<string>)ViewState["listaAutores"];
+		gvAutor.DataBind();
+	}
+
+	protected void gvAutor_RowDeleted(object sender, GridViewDeletedEventArgs e)
+	{
+	}
+
+	protected void gvAutor_RowDeleting(object sender, GridViewDeleteEventArgs e)
+	{
+		List<string> listaAutores = (List<string>)ViewState["listaAutores"];
+		listaAutores.RemoveAt(e.RowIndex);
+		gvAutor.DataSource = (List<string>)ViewState["listaAutores"];
+		gvAutor.DataBind();
+	}
+
+	protected void gvAutor_PageIndexChanging(object sender, GridViewPageEventArgs e)
+	{
+		gvAutor.PageIndex = e.NewPageIndex;
+	}
 }
