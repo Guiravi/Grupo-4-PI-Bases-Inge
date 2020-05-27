3d9a726c3ebb19aaad4b81d39be5cc715bf8d1b9
Marco Mora <marco8mora@gmail.com>
2020-05-27 03:30:36 -0600

    Se agregan formas de hacer la interfaz de Buscar más dinámicas

--- LaCafeteria/LaCafeteria/Pages/Buscar.cshtml.cs
+++ LaCafeteria/LaCafeteria/Pages/Buscar.cshtml.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using LaCafeteria.Models;
 
 namespace LaCafeteria.Pages
 {
     public class BuscarModel : PageModel
     {
+        public List<TopicoModel> Topicos { get; set; }
+
+        [BindProperty]
+        public int tiposArticulo { get; set; }
+
+        [BindProperty]
+        public string tipoBusqueda { get; set; }
+
         public void OnGet()
         {
 
         }
+
+        public IActionResult OnPost()
+        {
+            return Page();
+        }
+
     }
+
 }
---
+++ LaCafeteria/LaCafeteria/wwwroot/js/Buscar.js
@@ -0,0 +1,85 @@
+﻿let topicosBtn = document.getElementById('topicos');
+let titulosBtn = document.getElementById('titulos');
+let autoresBtn = document.getElementById('autores');
+let barraBusq = document.getElementById('barraBusqueda');
+let topicosDrop = document.getElementById('topicosDropdown');
+let buscarPorBtn = document.getElementById('buscarPor');
+let aceptarBtn = document.getElementById('aceptar');
+let barra = document.getElementById('barra');
+let dropdown = document.getElementById('dropdown');
+
+
+
+topicosBtn.addEventListener('click', topicosBtnClick);
+titulosBtn.addEventListener('click', titulosBtnClick);
+autoresBtn.addEventListener('click', autoresBtnClick);
+
+setEnterSubmit();
+
+
+
+
+function topicosBtnClick() {
+    topicosDrop.style.display = 'initial';
+    barraBusq.style.display = 'none';
+    buscarPorBtn.textContent = "Tópicos";
+    aceptarBtn.style.display = "initial";
+    aceptarBtn.style.position = "relative";
+    aceptarBtn.style.right = "300px";
+    dropdown.focus();
+} 
+
+function titulosBtnClick() {
+    topicosDrop.style.display = 'none';
+    barraBusq.style.display = 'initial';
+    buscarPorBtn.textContent = "Títulos";
+    aceptarBtn.style.display = "initial";
+    aceptarBtn.style.position = "initial";
+    barra.focus();
+} 
+
+function autoresBtnClick() {
+    topicosDrop.style.display = 'none';
+    barraBusq.style.display = 'initial';
+    buscarPorBtn.textContent = "Autores";
+    aceptarBtn.style.display = "initial";
+    aceptarBtn.style.position = "initial";
+    barra.focus();
+} 
+
+function enterSubmit() {
+    aceptarBtn.click();
+}
+
+function setEnterSubmit() {
+    window.onkeydown = function (e) {
+        if (e.keyCode === 13 && document.activeElement === barra) {
+            aceptarBtn.click();
+        }
+    }
+}
+
+
+function DoAjaxPostAndMore(btnClicked)
+{
+       var $form = $(btnClicked).parents('form');
+
+        $.ajax({
+            type: "POST",
+            url: $form.attr('action'),
+            data: $form.serialize(),
+            error: function(xhr, status, error) {
+                  //do something about the error
+             },
+            success: function(response) {
+                 //do something with response
+
+            }
+        });
+
+  return false;// if it's a link to prevent post
+
+}
+
+
+
