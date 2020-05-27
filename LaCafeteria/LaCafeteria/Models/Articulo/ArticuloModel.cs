0e93e58596881af097b52f52e17d836f8cd01dc5
KewQiu <7310376+KewQiu@users.noreply.github.com>
2020-05-27 01:54:51 -0600

    Interfaz de VerResumen en Razor Pages Listo

---
+++ LaCafeteria/LaCafeteria/Models/Articulo/ArticuloModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaCafeteria.Models
+{
+    public class ArticuloModel
+    {
+
+        public int idArticuloPK { get; set; }
+        public String titulo { get; set; }
+        public String resumen { get; set; }
+        public int tipo { get; set; }
+        public String contenido { get; set; }
+        public String fechaPublicacion { get; set; }
+        public String nombreAutor { get; set; }
+        public String usernameFK { get; set; }
+
+        public ArticuloModel(int idArticulo, String titulo, String resumen, int tipo, String contenido, String fechaPublicacion, String nombreAutor, String usernameFK) {
+            this.idArticuloPK = idArticulo;
+            this.titulo = titulo;
+            this.resumen = resumen;
+            this.tipo = tipo;
+            this.contenido = contenido;
+            this.fechaPublicacion = fechaPublicacion;
+            this.nombreAutor = nombreAutor;
+            this.usernameFK = usernameFK;
+        }
+
+        public ArticuloModel(String titulo, String resumen, int tipo, String contenido, String fechaPublicacion, String nombreAutor, String usernameFK) {
+            this.titulo = titulo;
+            this.resumen = resumen;
+            this.tipo = tipo;
+            this.contenido = contenido;
+            this.fechaPublicacion = fechaPublicacion;
+            this.nombreAutor = nombreAutor;
+            this.usernameFK = usernameFK;
+        }
+    }
+}
---
+++ LaCafeteria/LaCafeteria/Pages/VerResumen.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using LaCafeteria.Models;
+
+namespace LaCafeteria.Pages
+{
+    public class VerResumenModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public int idArticulo { get; set; }
+
+        //[BindProperty]
+        //public ArticuloModel articulo { get; set; }
+
+        public string articulo_titulo { get; set; }
+
+        public void OnGet() {
+
+            articulo_titulo = "Titulo de artículo";
+        }
+
+        public void OnPost() {
+
+        }
+    }
+}
