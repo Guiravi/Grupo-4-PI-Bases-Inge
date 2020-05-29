3400de4b12c455bb3ee9caf34cdfb7ae06c9dc58
Marco Mora <54404955+MarcoMora85@users.noreply.github.com>
2020-05-29 12:19:32 -0600

    Querries de datos dummy para distintas tablas
    No incluye dummies para Artículo

---
+++ QuerriesIteracion2/TopicoDummies.sql
@@ -0,0 +1,17 @@
+INSERT INTO [dbo].[Topico]
+VALUES ('Biología')
+
+INSERT INTO [dbo].[Topico]
+VALUES ('Ciencias Sociales')
+
+INSERT INTO [dbo].[Topico]
+VALUES ('Economía')
+
+INSERT INTO [dbo].[Topico]
+VALUES ('Física')
+
+INSERT INTO [dbo].[Topico]
+VALUES ('Política')
+
+INSERT INTO [dbo].[Topico]
+VALUES ('Química')
