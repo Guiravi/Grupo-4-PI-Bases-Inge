a73f2a4eebb07263b9ccb3eb3783b43b134cf818
KewQiu <7310376+KewQiu@users.noreply.github.com>
2020-05-28 22:46:11 -0600

    Introduccion de Modelo a VerResumen

--- LaCafeteria/LaCafeteria/Pages/VerResumen.cshtml.cs
+++ LaCafeteria/LaCafeteria/Pages/VerResumen.cshtml.cs
@@ -5,22 +5,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using LaCafeteria.Models;
+using LaCafeteria.Controllers;
 
 namespace LaCafeteria.Pages
 {
     public class VerResumenModel : PageModel
     {
         [BindProperty(SupportsGet = true)]
-        public int idArticulo { get; set; }
+        public int idArticuloPK { get; set; }
 
-        //[BindProperty]
-        //public ArticuloModel articulo { get; set; }
+        [BindProperty]
+        public ArticuloModel articulo { get; set; }
 
-        public string articulo_titulo { get; set; }
+        public ArticuloController articuloController { get; set; }
+
+        public TopicoController topicoController { get; set; }
+
+        public string topicos { get; set; }
+
+        public string autores { get; set; }
 
         public void OnGet() {
+            articulo = new ArticuloModel();
+            articuloController = new ArticuloController();
+            topicoController = new TopicoController();
 
-            articulo_titulo = "Titulo de artículo";
+            
         }
 
         public void OnPost() {
