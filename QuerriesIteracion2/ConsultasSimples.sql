5784e6680da98b4bd2dd52235f813fe55f916bb8
juan herrera <jjherrerarodriguez98@gmail.com>
2020-06-01 18:11:57 -0600

    Se adjunta la modificación de los códigos de las consultas simples

---
+++ QuerriesIteracion2/ConsultasSimples.sql
@@ -0,0 +1,27 @@
+﻿/*Consulta simple 1*/
+SELECT usernamePK
+, email
+, nombre
+, apellido1
+, apellido2
+, fechaNacimiento
+, pais
+, estado
+, ciudad
+, rutaImagenPerfil
+, hobbies
+, habilidades
+, idiomas
+, informacionLaboral
+, meritos
+, activo
+, nombreRolFK
+FROM Miembro
+/*Consulta simple 2*/
+SELECT Topico.nombrePK FROM Topico
+/*Consulta simple 3*/
+SELECT A.idArticuloPK, A.titulo, A.tipo, A.fechaPublicacion, A.resumen, A.contenido, A.estado, A.visitas, A.puntajeTotalRev, A.calificacionTotalMiem  
+FROM  Articulo A 
+JOIN MiembroAutorDeArticulo MAA 
+ON A.idArticuloPK = MAA.idArticuloFK 
+WHERE usernameMiemFK = @username
