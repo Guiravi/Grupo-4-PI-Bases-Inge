8f864c862e158cfedaf06496f518716464e54057
Silvionsky <silviosalac@gmail.com>
2020-06-01 19:01:19 -0600

    Comentarios para explicar que hacen las consultas simples y para que se utilizan

--- QuerriesIteracion2/ConsultasSimples.sql
+++ QuerriesIteracion2/ConsultasSimples.sql
@@ -1,4 +1,7 @@
-﻿/*Consulta simple 1*/
+-- Consulta simple 1
+-- Esta consulta recupera todos los atributos para cada uno de los miembros.
+-- Se utiliza en la solución para la historia de prioridad 1 (Escribir articulo) para desplegar los miembros en la interfaz de usuario
+-- y que el usuario pueda seleccionar autores del articulo.
 SELECT usernamePK
 , email
 , nombre
@@ -17,9 +20,17 @@ SELECT usernamePK
 , activo
 , nombreRolFK
 FROM Miembro
-/*Consulta simple 2*/
+
+-- Consulta simple 2
+-- Esta consulta recupera la llave primaria (nombrePK) de todos los topicos.
+-- Se utiliza para desplegar todos los topicos que existen en la interfaz de usuario para las historias de prioridad 1 (Escribir articulo)
+-- y prioridad 7(Navegar y buscar)
 SELECT Topico.nombrePK FROM Topico
-/*Consulta simple 3*/
+
+-- Consulta simple 3
+-- Esta consulta recupera todos los articulos de un miembro
+-- Se utiliza para la solucion de la historia con prioridad 4 (Acceder y editar mis articulos). Los articulos del miembro se despliegan
+-- en una interfaz para que este los pueda leer y editar.
 SELECT A.idArticuloPK, A.titulo, A.tipo, A.fechaPublicacion, A.resumen, A.contenido, A.estado, A.visitas, A.puntajeTotalRev, A.calificacionTotalMiem  
 FROM  Articulo A 
 JOIN MiembroAutorDeArticulo MAA 
