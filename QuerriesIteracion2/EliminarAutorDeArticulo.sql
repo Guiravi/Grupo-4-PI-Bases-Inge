794bddda9c239f34dee589b3260091a96b704240
Guiravi <62527880+Guiravi@users.noreply.github.com>
2020-06-02 23:44:46 -0600

    se verifica la consulta
    se asegura que el artículo esté publicado

--- QuerriesIteracion2/EliminarAutorDeArticulo.sql
+++ QuerriesIteracion2/EliminarAutorDeArticulo.sql
@@ -4,12 +4,15 @@ AFTER DELETE
 AS
 DECLARE @idArticuloFK INT
 DECLARE @usernameMiemFK nvarchar(20)
+DECLARE @estado nvarchar(30)
 SELECT @idArticuloFK = D.idArticuloFK FROM deleted D
 SELECT @usernameMiemFK = D.usernameMiemFK FROM deleted D
-BEGIN
-	UPDATE Miembro
-	SET meritos = meritos - (SELECT puntajeTotalRev
-								FROM [dbo].Articulo 
-								WHERE @idArticuloFK = [dbo].Articulo.idArticuloPK)
-	WHERE usernamePK = @usernameMiemFK
-END
+SELECT @estado = Articulo.estado FROM Articulo WHERE idArticuloPK = @idArticuloFK
+IF ( @estado = 'Publicado')
+	BEGIN
+		UPDATE Miembro
+		SET meritos = meritos - (SELECT puntajeTotalRev
+									FROM [dbo].Articulo 
+									WHERE @idArticuloFK = [dbo].Articulo.idArticuloPK)
+		WHERE usernamePK = @usernameMiemFK
+	END
