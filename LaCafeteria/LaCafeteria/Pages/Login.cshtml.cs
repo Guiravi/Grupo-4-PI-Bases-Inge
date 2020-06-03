294c6fccaeb5eba80f8dec937eefabd678963c4d
Silvio Salazar <silviosalac@gmail.com>
2020-06-02 19:18:23 -0600

    Login creado. Se inicializa un cookie con el nombre de username logeado. El usuario puede iniciar sesion (sin atenticacion real) y cerrar sesion.

---
+++ LaCafeteria/LaCafeteria/Pages/Login.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using LaCafeteria.Utilidades;
+
+namespace LaCafeteria.Pages
+{
+	public class LoginModel : PageModel
+	{	
+		[Required(ErrorMessage = "Debe ingresar su nombre de usuario")]
+		[BindProperty]
+		public string usernamePK { set; get; }
+
+		[BindProperty(SupportsGet = true)]
+		public string cerrarSesion { set; get; }
+
+		public IActionResult OnGet()
+        {
+			if(cerrarSesion != null)
+			{
+				Response.Cookies.Delete("usernamePK");
+				Notificaciones.Set(this, "cerrarSesion", "Se ha cerrado la sesión", Notificaciones.TipoNotificacion.Exito);
+				return Redirect("/Login");
+			}
+
+			return Page();
+		}
+
+		public IActionResult OnPost()
+		{
+			// TODO: Logica de validacion para el username y password
+			if(ModelState.IsValid)
+			{
+				Response.Cookies.Append("usernamePK", usernamePK);
+				return Redirect("/Index");
+			}
+
+			return Page();
+		}
+    }
+}
