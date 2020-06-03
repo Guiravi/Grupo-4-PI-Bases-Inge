9ce9b4c9b62af4574c341de65494e50de152f5bc
Silvio Salazar <silviosalac@gmail.com>
2020-06-03 01:11:37 -0600

    VerMiembros despliega la lista de todos los miembros.

--- LaCafeteria/LaCafeteria/Pages/VerMiembros.cshtml.cs
+++ LaCafeteria/LaCafeteria/Pages/VerMiembros.cshtml.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using LaCafeteria.Models;
+using LaCafeteria.Controllers;
+using LaCafeteria.Utilidades;
 
 namespace LaCafeteria.Pages
 {
-    public class VerMiembrosModel : PageModel
-    {
+	public class VerMiembrosModel : PageModel
+	{
+		public List<MiembroModel> miembros { get; set; }
+		public MiembroController miembroController;
+
+		public VerMiembrosModel()
+		{
+			miembroController = new MiembroController();
+		}
+
         public void OnGet()
         {
-
+			miembros = miembroController.GetListaMiembros();
         }
     }
 }
