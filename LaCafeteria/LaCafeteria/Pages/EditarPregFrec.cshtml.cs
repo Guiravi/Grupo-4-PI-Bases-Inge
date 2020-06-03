a79c87f8a7307aa26008769e5a81e38d01d9d54c
juan herrera <jjherrerarodriguez98@gmail.com>
2020-06-03 00:07:10 -0600

    Se agregan notifiicaciones para indicar condiciones de éxito o fallos de Editar Preguntas Frecuentes

--- LaCafeteria/LaCafeteria/Pages/EditarPregFrec.cshtml.cs
+++ LaCafeteria/LaCafeteria/Pages/EditarPregFrec.cshtml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using LaCafeteria.Controllers;
+using LaCafeteria.Utilidades;
 
 namespace LaCafeteria.Pages
 {
@@ -36,23 +37,21 @@ namespace LaCafeteria.Pages
         }
         public void OnPostDelete()
         {
-            //Request.Form["emailaddress"];
-            //Request.Form["emailaddress"];
-            //Request.Form["emailaddress"];
+           
             this.error = fAQsController.Borrar(categoria, pregunta, respuesta);
             switch (error)
             {
                 case 0:
-                    TempData["Message"] = "Se ha eliminado la pregunta y la respuesta con éxito en su respectiva categoría";
+                    Notificaciones.Set(this, "exitoEliminar", "Se ha eliminado la pregunta y la respuesta con éxito en su respectiva categoría", Notificaciones.TipoNotificacion.Exito);
+                    //TempData["Message"] = "Se ha eliminado la pregunta y la respuesta con éxito en su respectiva categoría";
                     break;
                 case 1:
-                    TempData["Message"] = "Por favor complete todos los campos antes de solicitar una funcionalidad";
+                    Notificaciones.Set(this, "camposIncompletos", "Por favor complete todos los campos antes de solicitar una funcionalidad", Notificaciones.TipoNotificacion.Error);
+                    //TempData["Message"] = "Por favor complete todos los campos antes de solicitar una funcionalidad";
                     break;
                 case 3:
-                    TempData["Message"] = "Por favor ingrese otra pregunta y respuesta, ya que su pregunta no se ecuentra para eliminarla";
-                    break;
-                default:
-                    TempData["Message"] = "";
+                    Notificaciones.Set(this, "noEncontrados", "Por favor ingrese otra pregunta y respuesta, ya que su pregunta no se ecuentra para eliminarla", Notificaciones.TipoNotificacion.Error);
+                    //TempData["Message"] = "Por favor ingrese otra pregunta y respuesta, ya que su pregunta no se ecuentra para eliminarla";
                     break;
             }
         }
@@ -61,17 +60,19 @@ namespace LaCafeteria.Pages
             this.error = fAQsController.Agregar(categoria, pregunta, respuesta);
             switch (error) {
                 case 0:
-                    TempData["Message"] = "Se ha agregado la pregunta y la respuesta con éxito en su respectiva categoría";
+                    Notificaciones.Set(this, "exitoAgregar", "Se ha agregado la pregunta y la respuesta con éxito en su respectiva categoría", Notificaciones.TipoNotificacion.Exito);
+                    //TempData["Message"] = "Se ha agregado la pregunta y la respuesta con éxito en su respectiva categoría";
                     break;
                 case 1:
-                    TempData["Message"] = "Por favor complete todos los campos antes de solicitar una funcionalidad";
+                    Notificaciones.Set(this, "camposIncompletos", "Por favor complete todos los campos antes de solicitar una funcionalidad", Notificaciones.TipoNotificacion.Error);
+                    //TempData["Message"] = "Por favor complete todos los campos antes de solicitar una funcionalidad";
                     break;
                 case 2:
-                    TempData["Message"] = "Por favor ingrese otra pregunta y respuesta, ya que su pregunta está repetida para la categoría seleccionada";
+                    Notificaciones.Set(this,"camposRepetidos" ,"Por favor ingrese otra pregunta y respuesta, ya que su pregunta está repetida para la categoría seleccionada", Notificaciones.TipoNotificacion.Error);
+
+                    //TempData["Message"] = "Por favor ingrese otra pregunta y respuesta, ya que su pregunta está repetida para la categoría seleccionada";
                     break;
-                default:
-                    TempData["Message"] = "";
-                    break;
+
             }
         }
 
