67e682e908f6beb3b62d7da3cb0af9fcb3f9ce2b
Silvio Salazar <silviosalac@gmail.com>
2020-06-03 15:57:06 -0600

    Se almacenan y despliegan correctamente las imagenes de perfil de los miembros

--- LaCafeteria/LaCafeteria/Pages/CrearPerfil.cshtml.cs
+++ LaCafeteria/LaCafeteria/Pages/CrearPerfil.cshtml.cs
@@ -23,6 +23,9 @@ namespace LaCafeteria.Pages
 		[Required(ErrorMessage = "Debe subir una imagen de perfil")]
 		public IFormFile imagenDePerfil { set; get; }
 
+		[BindProperty]
+		public List<string> listaIdiomas { set; get; }
+
 		public MiembroController miembroController{ set; get; }
 
 		public string rutaCarpeta;
@@ -30,6 +33,7 @@ namespace LaCafeteria.Pages
 		public CrearPerfilModel(IHostingEnvironment env)
 		{
 			miembroController = new MiembroController();
+			listaIdiomas = new List<string>();
 			rutaCarpeta = env.WebRootPath;
 		}
 
@@ -37,14 +41,14 @@ namespace LaCafeteria.Pages
         {
 			if(EsValido())
 			{
-				var filePath = rutaCarpeta + "/ImagenesPerfil/" + miembro.usernamePK + "." + imagenDePerfil.ContentType.Split('/')[1];
+				var filePath = rutaCarpeta + "/images/ImagenesPerfil/" + miembro.usernamePK + "." + imagenDePerfil.ContentType.Split('/')[1];
 
 				using (var stream = System.IO.File.Create(filePath))
 				{
 					imagenDePerfil.CopyTo(stream);
 				}
 
-				miembro.rutaImagenPerfil = "/ImagenesPerfil/" + miembro.usernamePK + ".jpg";
+				miembro.rutaImagenPerfil = "images/ImagenesPerfil/" + miembro.usernamePK + "." + imagenDePerfil.ContentType.Split('/')[1];
 				miembroController.CrearMiembro(miembro);
 				Response.Cookies.Append("usernamePK", miembro.usernamePK);
 				Notificaciones.Set(this, "sesionIniciada", "Sesión iniciada", Notificaciones.TipoNotificacion.Exito);
