9ff09f6d7c79236852c4fbd7354496a906f9b5d3
Silvio Salazar <silviosalac@gmail.com>
2020-06-25 20:31:50 -0600

    [Notificaciones] Modelo Notificacion agregado

--- LaCafeteria/LaCafeteria/Models/Notificacion.cs
+++ LaCafeteria/LaCafeteria/Models/Notificacion.cs
@@ -7,6 +7,24 @@ namespace LaCafeteria.Models
 {
 	public class Notificacion
 	{
+		public static string Nueva = "Nueva";
+		public static string Leida = "Leída";
 
+		public int notificacionID { get; set; }
+		public string usernameFK { get; set; }
+		public string fechaCreacion { get; set; }
+		public string mensaje { get; set; }
+		public string estado { get; set; }
+		public string url { get; set; }
+
+		public Notificacion(string usernameFK, string mensaje, string url)
+		{
+			notificacionID = -1;
+			this.usernameFK = usernameFK;
+			this.fechaCreacion = DateTime.Now.ToShortDateString();
+			this.mensaje = mensaje;
+			this.estado = Nueva;
+			this.url = url;
+		}
 	}
 }
