6dba448d02a4f5280fa3ec9cd39885372b666a4f
KewQiu <7310376+KewQiu@users.noreply.github.com>
2020-06-26 00:45:05 -0600

    [Refactoring] Se crea clase EditorMiembroDBHandler y se traslada metodo ActualizarMiembro

--- LaCafeteria/LaCafeteria/Models/MiembroDBHandler.cs
+++ LaCafeteria/LaCafeteria/Models/MiembroDBHandler.cs
@@ -36,46 +36,7 @@ namespace LaCafeteria.Models
 
 
 
-        public void ActualizarMiembro(string usernamePK, MiembroModel miembro)
-        {
 
-            string connectionString = AppSettings.GetConnectionString();
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                string sqlString = @"UPDATE Miembro SET idiomas = @idiomas,
-                                                        hobbies = @hobbies, 
-                                                        habilidades = @habilidades
-									WHERE usernamePK = @usernamePK ";
-                SqlCommand command = new SqlCommand(sqlString, connection);
-                command.Parameters.AddWithValue("@usernamePK", usernamePK);
-                if (miembro.hobbies != null)
-                {
-                    command.Parameters.AddWithValue("@hobbies", miembro.hobbies);
-                }
-                else
-                {
-                    command.Parameters.AddWithValue("@hobbies", DBNull.Value);
-                }
-                if (miembro.habilidades != null)
-                {
-                    command.Parameters.AddWithValue("@habilidades", miembro.habilidades);
-                }
-                else
-                {
-                    command.Parameters.AddWithValue("@habilidades", DBNull.Value);
-                }
-                if (miembro.idiomas != null)
-                {
-                    command.Parameters.AddWithValue("@idiomas", miembro.idiomas);
-                }
-                else
-                {
-                    command.Parameters.AddWithValue("@idiomas", DBNull.Value);
-                }
-                command.ExecuteNonQuery();
-            }
-        }
 
 
 
