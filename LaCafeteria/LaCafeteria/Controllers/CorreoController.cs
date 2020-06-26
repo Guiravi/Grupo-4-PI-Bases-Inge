07573845b153099e1c30c5a25e5d8c21a09eb0b9
KewQiu <7310376+KewQiu@users.noreply.github.com>
2020-06-26 04:13:43 -0600

    [Refactoring] Se crea clase EnviarEmailController con constructor y se traslada metodo EnviarMail

--- LaCafeteria/LaCafeteria/Controllers/CorreoController.cs
+++ LaCafeteria/LaCafeteria/Controllers/CorreoController.cs
@@ -16,98 +16,14 @@ namespace LaCafeteria.Controllers
     public class CorreoController
     {
         public MiembroDBHandler miembroDBHandler;
-        public List<MiembroModel> listaMiembros;
+
         public List<MiembroModel> listaNucleos;
 
-        public string rutaCarpeta { get; set; }
-
-        public CorreoController(IHostingEnvironment env) {
-            miembroDBHandler = new MiembroDBHandler();
-            listaMiembros = miembroDBHandler.GetListaMiembros();
-            rutaCarpeta = env.WebRootPath;
-        }
 
 
 
-        public void sendMail(string destino, string remitente, string asunto, string mensaje, string filePath) {
-            string usernameDestino = "";
-            MiembroModel miembroDestino;
-            MiembroModel miembroRemitente;
-            string nombreCompletoDestino = "";
-            string nombreCompletoRemitente = "";
-            string emailAddress = "";
-
-            miembroRemitente = listaMiembros.Find(x => x.usernamePK == remitente);
-            nombreCompletoRemitente = miembroRemitente.nombre + " " + miembroRemitente.apellido1 + " " + miembroRemitente.apellido2;
-
-            if ( !destino.Contains("@") )
-            {
-                usernameDestino = destino.Split('(', ')')[1];
-
-                miembroDestino = listaMiembros.Find(x => x.usernamePK == usernameDestino);
-
-                nombreCompletoDestino = miembroDestino.nombre + " " + miembroDestino.apellido1 + " " + miembroDestino.apellido2;
-                emailAddress = miembroDestino.email;
-            } else
-            {
-                emailAddress = destino;
-                nombreCompletoDestino = destino;
-            }
-
-            asunto = "[LaCafeteria] " + asunto;
-            string mensajeHtml = "<h3>Estimado " + nombreCompletoDestino + ",</h3>" +
-                "<p><i>Has recibido un mensaje de la comunidad La Cafeteria del miembro " + nombreCompletoRemitente + " (" + remitente + ") : </i><br/><br/>" + mensaje +
-                "<br/><br/><i> Para responder, por favor hacerlo desde nuestro sitio web. </i><br/><br/></p>";
-
-            MailAddress fromAddress = new MailAddress("thecoffeeplace2020@gmail.com", "La Cafeteria");
-            MailAddress toAddress = new MailAddress(emailAddress, nombreCompletoDestino);
-
-            const string fromPassword = "thecoffeeplace";
-
-            var smtp = new SmtpClient
-            {
-                Host = "smtp.gmail.com",
-                Port = 587,
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
-            };
-
-            LinkedResource resource = new LinkedResource(rutaCarpeta + "/images/TCP.png", "image/png");
-            resource.ContentId = Guid.NewGuid().ToString();
-            string htmlImage = @"<img src='cid:" + resource.ContentId + @"'/>";
-
-            AlternateView htmlView = AlternateView.CreateAlternateViewFromString(mensajeHtml + htmlImage, Encoding.UTF8, MediaTypeNames.Text.Html);
-            htmlView.LinkedResources.Add(resource);
-
-            var message = new MailMessage(fromAddress, toAddress);
-
-            message.Subject = asunto;
-            message.IsBodyHtml = true;
-
-            if ( filePath != "" )
-            {
-                System.Net.Mail.Attachment attachment;
-                attachment = new System.Net.Mail.Attachment(filePath);
-                message.Attachments.Add(attachment);
-            }
-
-            message.AlternateViews.Add(htmlView);
-            smtp.Send(message);
 
 
-
-            /*if ( filePath != "" )
-            {
-                message.Attachments.Clear();
-                message.Attachments.Dispose();
-                message.Dispose();
-                message = null;
-                smtp.Dispose();
-                System.IO.File.Delete(filePath);
-            }*/
-        }
 
 
         public void sendNecesitaRevision(string titulo) {
