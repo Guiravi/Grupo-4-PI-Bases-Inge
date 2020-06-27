d00f00a79f60afaf047423d2c84e6ca8373dc202
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-06-27 13:14:57 -0600

    [Enviar opinion] Se hace el controlador de para la revisión del artículo

--- LaCafeteria/LaCafeteria/Controllers/RevisorArticulosController.cs
+++ LaCafeteria/LaCafeteria/Controllers/RevisorArticulosController.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using LaCafeteria.Models;
+using LaCafeteria.Models.Handlers;
 
 namespace LaCafeteria.Controllers
 {
     public class RevisorArticulosController
     {
-        private ;
-
-        public void ActualizarRevisionArticulo()
+        RevisorArticuloHandler revisorArticuloHandler;
+        
+        RevisorArticulosController()
         {
 
+        }
+
+        public void ActualizarRevisionArticulo(int opinion, int contribucion, int forma, string estadoRevision,
+                                                    string comentarios, string recomendacion, string username, int idArticulo)
+        {
+            revisorArticuloHandler.ActualizarEstadoRevisionArticulo(opinion, contribucion, forma, estadoRevision,
+                                                    comentarios, recomendacion, username, idArticulo);
         }
     }
 }
