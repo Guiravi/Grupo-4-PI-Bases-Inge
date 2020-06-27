cff70a1d5ec9f33a10f1b7772f8580ca520bdbb2
KewQiu <7310376+KewQiu@users.noreply.github.com>
2020-06-27 02:10:15 -0600

    [Refactoring] Se corrige logica de vista ModificarPerfil debido a la nueva BD

--- LaCafeteria/LaCafeteria/Pages/ModificarPerfil.cshtml.cs
+++ LaCafeteria/LaCafeteria/Pages/ModificarPerfil.cshtml.cs
@@ -15,9 +15,9 @@ namespace LaCafeteria.Pages
         [BindProperty]
         public MiembroModel miembro { set; get; }
 
-        //public MiembroController miembroController { set; get; }
         private BuscadorMiembrosController buscadorMiembrosController;
         private EditorMiembroController editorArticuloController;
+        private InformacionCatalogosController informacionCatalogosController;
 
         public List<string> listaIdiomasElegidos { set; get; }
 
@@ -29,57 +29,25 @@ namespace LaCafeteria.Pages
         {
             buscadorMiembrosController = new BuscadorMiembrosController();
             editorArticuloController = new EditorMiembroController();
+            informacionCatalogosController = new InformacionCatalogosController();
 
             listaIdiomasElegidos = new List<string>();
-            listaIdiomasDisponibles = new List<string>();
+            listaIdiomasDisponibles = informacionCatalogosController.GetIdiomasCatalogo();
         }
 
         public void OnGet()
         {
             miembro = buscadorMiembrosController.GetMiembro(Request.Cookies["usernamePK"]);
-            listaIdiomasDisponibles.Add("Español");
-            listaIdiomasDisponibles.Add("Inglés");
-            listaIdiomasDisponibles.Add("Árabe");
-            listaIdiomasDisponibles.Add("Chino");
-            listaIdiomasDisponibles.Add("Ruso");
-            string idiomas = miembro.idiomas;
-            //string idiomas = "Español,Chino,Inglés";
-            if (idiomas != null)
-            {
-                listaIdiomasElegidos = idiomas.Split(',').ToList();
-                while (listaIdiomasElegidos.Count() < listaIdiomasDisponibles.Count())
-                {
-                    listaIdiomasElegidos.Add("");
-                }
-            }
-            
+            listaIdiomasElegidos = miembro.idiomas;
         }
         
         public IActionResult OnPostActualizar()
         {
-            miembro.idiomas = ObtenerIdiomasCSV();
+            miembro.idiomas = listaIdiomas;
             editorArticuloController.ActualizarMiembro(Request.Cookies["usernamePK"], miembro);
             Notificaciones.Set(this, "Actualizado", "Su perfil se ha actualizado satifactoriamente", Notificaciones.TipoNotificacion.Exito);
             return Redirect("/MiPerfil");
 
         }
-
-        public string ObtenerIdiomasCSV()
-        {
-            string idiomas = null;
-
-            if (listaIdiomas.Count > 0)
-            {
-                idiomas = "";
-                foreach (string idioma in listaIdiomas)
-                {
-                    idiomas += idioma + ",";
-                }
-                idiomas = idiomas.TrimEnd(',');
-            }
-
-            return idiomas;
-        }
-
     }
 }
