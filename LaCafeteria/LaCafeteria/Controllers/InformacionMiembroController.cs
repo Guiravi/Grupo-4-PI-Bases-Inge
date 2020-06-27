29b558be57a20bbd6a63584ae58318a4d14c21dc
Marco Mora <marco8mora@gmail.com>
2020-06-27 04:29:33 -0600

    [VerReporteFiltrable] Se agregan las funciones de InformacionMiembroController  que devuelven los datos de los gráficos para la historia 7

--- LaCafeteria/LaCafeteria/Controllers/InformacionMiembroController.cs
+++ LaCafeteria/LaCafeteria/Controllers/InformacionMiembroController.cs
@@ -23,5 +23,67 @@ namespace LaCafeteria.Controllers
 
 			return listaNotificaciones;
 		}
-	}
+
+        public List<DatosGraficoDona> GetMiembrosPorPais()
+        {
+            return informacionMiembroDBHandler.GetMiembrosPorPais();
+        }
+
+        public List<DatosGraficoBarrasApilado> GetHabilidadesPorPais()
+        {
+            List<DatosGraficoBarrasApilado> lista = informacionMiembroDBHandler.GetHabilidadesPorPais();
+            List<string> listaSinAsignar = informacionMiembroDBHandler.GetHabilidadesEstandarSinAsignar();
+
+            for (int i = 0; i < listaSinAsignar.Count; i++)
+            {
+                DatosGraficoBarrasApilado habilidad = new DatosGraficoBarrasApilado(listaSinAsignar[i], "", 0);
+                lista.Add(habilidad);
+            }
+
+            return lista;
+        }
+
+        public List<DatosGraficoBarrasApilado> GetHabilidadesPorIdioma()
+        {
+            List<DatosGraficoBarrasApilado> lista = informacionMiembroDBHandler.GetHabilidadesPorIdioma();
+            List<string> listaSinAsignar = informacionMiembroDBHandler.GetHabilidadesEstandarSinAsignar();
+
+            for (int i = 0; i < listaSinAsignar.Count; i++)
+            {
+                DatosGraficoBarrasApilado habilidad = new DatosGraficoBarrasApilado(listaSinAsignar[i], "", 0);
+                lista.Add(habilidad);
+            }
+
+            return lista;
+        }
+
+        public List<DatosGraficoBarrasApilado> GetPasatiemposPorPais()
+        {
+            List<DatosGraficoBarrasApilado> lista = informacionMiembroDBHandler.GetPasatiemposPorPais();
+            List<string> listaSinAsignar = informacionMiembroDBHandler.GetPasatiemposEstandarSinAsignar();
+
+            for (int i = 0; i < listaSinAsignar.Count; i++)
+            {
+                DatosGraficoBarrasApilado pasatiempo = new DatosGraficoBarrasApilado(listaSinAsignar[i], "", 0);
+                lista.Add(pasatiempo);
+            }
+
+            return lista;
+        }
+
+        public List<DatosGraficoBarrasApilado> GetPasatiemposPorIdioma()
+        {
+            List<DatosGraficoBarrasApilado> lista = informacionMiembroDBHandler.GetPastiemposPorIdioma();
+            List<string> listaSinAsignar = informacionMiembroDBHandler.GetPasatiemposEstandarSinAsignar();
+
+            for (int i = 0; i < listaSinAsignar.Count; i++)
+            {
+                DatosGraficoBarrasApilado pasatiempo = new DatosGraficoBarrasApilado(listaSinAsignar[i], "", 0);
+                lista.Add(pasatiempo);
+            }
+
+            return lista;
+        }
+
+    }
 }
