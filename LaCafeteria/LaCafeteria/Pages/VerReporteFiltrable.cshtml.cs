3752c48462b7aaeef723cbad5ff44e45b2db1a62
Marco Mora <marco8mora@gmail.com>
2020-06-27 00:44:35 -0600

    [VerReporteFiltrable] Código de lógica para que los gráficos de ambos reportes desplieguen los datos de la BD.

--- LaCafeteria/LaCafeteria/Pages/VerReporteFiltrable.cshtml.cs
+++ LaCafeteria/LaCafeteria/Pages/VerReporteFiltrable.cshtml.cs
@@ -9,9 +9,47 @@ namespace LaCafeteria.Pages
 {
     public class VerReporteFiltrableModel : PageModel
     {
+        public List<DatosGraficoDona> miembrosPorPais;
+        public List<DatosGraficoBarrasApilado> habilidadesPorIdioma;
+        public List<DatosGraficoBarrasApilado> habilidadesPorPais;
+        public List<DatosGraficoBarrasApilado> pasatiemposPorIdioma;
+        public List<DatosGraficoBarrasApilado> pasatiemposPorPais;
+        public List<DatosGraficoDona> miembrosPorRol;
+        public List<DatosGraficoDona> articulosPorRol;
+
         public void OnGet()
         {
 
         }
+
+        public class DatosGraficoDona
+        {
+            public string categoria { get; set; }
+
+            public int cantidad { get; set; }
+
+            public DatosGraficoDona(string cat, int cant)
+            {
+                categoria = cat;
+                cantidad = cant;
+            }
+        }
+
+        public class DatosGraficoBarrasApilado
+        {
+            public string nombreCategoria { get; set; }
+
+            public string nombreSubcategoria { get; set; }
+
+            public int cantidad { get; set; }
+
+            public DatosGraficoBarrasApilado(string nomCat, string nomSub, int cant)
+            {
+                nombreCategoria = nomCat;
+                nombreSubcategoria = nomSub;
+                cantidad = cant;
+            }
+        }        
+       
     }
 }
