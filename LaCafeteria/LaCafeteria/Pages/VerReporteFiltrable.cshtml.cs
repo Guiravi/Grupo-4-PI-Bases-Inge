695545973201ac3985b7eebf94a8553c32c0b771
Marco Mora <marco8mora@gmail.com>
2020-06-27 05:06:14 -0600

    [VerReporteFiltrable] Se agregan los llamados a las funciones del Controller en la vista de VerReporteFiltrable

--- LaCafeteria/LaCafeteria/Pages/VerReporteFiltrable.cshtml.cs
+++ LaCafeteria/LaCafeteria/Pages/VerReporteFiltrable.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using LaCafeteria.Models;
+using LaCafeteria.Controllers;
 
 namespace LaCafeteria.Pages
 {
@@ -13,16 +14,35 @@ namespace LaCafeteria.Pages
         public List<DatosGraficoDona> miembrosPorPais;
         public List<DatosGraficoBarrasApilado> habilidadesPorIdioma;
         public List<DatosGraficoBarrasApilado> habilidadesPorPais;
-        public List<string> habilidadesSinAsignar;
         public List<DatosGraficoBarrasApilado> pasatiemposPorIdioma;
         public List<DatosGraficoBarrasApilado> pasatiemposPorPais;
-        public List<string> pasatiemposSinAsignar;
         public List<DatosGraficoDona> miembrosPorRol;
         public List<DatosGraficoDona> articulosPorRol;
 
+        private InformacionMiembroController informacionMiembroController;
+
+        public VerReporteFiltrableModel()
+        {
+            miembrosPorPais = new List<DatosGraficoDona>();
+            habilidadesPorPais = new List<DatosGraficoBarrasApilado>();
+            habilidadesPorIdioma = new List<DatosGraficoBarrasApilado>();
+            pasatiemposPorPais = new List<DatosGraficoBarrasApilado>();
+            pasatiemposPorIdioma = new List<DatosGraficoBarrasApilado>();
+            miembrosPorPais = new List<DatosGraficoDona>();
+            articulosPorRol = new List<DatosGraficoDona>();
+
+            informacionMiembroController = new InformacionMiembroController();
+
+            
+        }
+
         public void OnGet()
         {
-
+            miembrosPorPais = informacionMiembroController.GetMiembrosPorPais();
+            habilidadesPorPais = informacionMiembroController.GetHabilidadesPorPais();
+            habilidadesPorIdioma = informacionMiembroController.GetHabilidadesPorIdioma();
+            pasatiemposPorPais = informacionMiembroController.GetPasatiemposPorPais();
+            pasatiemposPorIdioma = informacionMiembroController.GetPasatiemposPorIdioma();
         }      
        
     }
