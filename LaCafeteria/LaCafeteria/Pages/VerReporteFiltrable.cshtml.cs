980bc02b51300f011720cda01082d3a542a7763b
Marco Mora <marco8mora@gmail.com>
2020-06-27 03:32:17 -0600

    [VerReporteFiltrable] Las clases de DatosGrafico pasaron al archivo de DatosGraficoModel

--- LaCafeteria/LaCafeteria/Pages/VerReporteFiltrable.cshtml.cs
+++ LaCafeteria/LaCafeteria/Pages/VerReporteFiltrable.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using LaCafeteria.Models;
 
 namespace LaCafeteria.Pages
 {
@@ -20,36 +21,7 @@ namespace LaCafeteria.Pages
         public void OnGet()
         {
 
-        }
-
-        public class DatosGraficoDona
-        {
-            public string categoria { get; set; }
-
-            public int cantidad { get; set; }
-
-            public DatosGraficoDona(string cat, int cant)
-            {
-                categoria = cat;
-                cantidad = cant;
-            }
-        }
-
-        public class DatosGraficoBarrasApilado
-        {
-            public string nombreCategoria { get; set; }
-
-            public string nombreSubcategoria { get; set; }
-
-            public int cantidad { get; set; }
-
-            public DatosGraficoBarrasApilado(string nomCat, string nomSub, int cant)
-            {
-                nombreCategoria = nomCat;
-                nombreSubcategoria = nomSub;
-                cantidad = cant;
-            }
-        }        
+        }      
        
     }
 }
