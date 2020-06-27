9e0c3d65793eb8e8d4f6463b8b9897a3e79b21d7
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-06-27 05:14:54 -0600

    [Enviar opinion] Se empieza a implementar el modelo revisor de articulos

---
+++ LaCafeteria/LaCafeteria/Controllers/RevisorArticulosController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LaCafeteria.Models;
+
+namespace LaCafeteria.Controllers
+{
+    public class RevisorArticulosController
+    {
+        private ;
+
+        public void ActualizarRevisionArticulo()
+        {
+
+        }
+    }
+}
--- LaCafeteria/LaCafeteria/Pages/RevisarArticulo.cshtml.cs
+++ LaCafeteria/LaCafeteria/Pages/RevisarArticulo.cshtml.cs
@@ -4,35 +4,55 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using LaCafeteria.Utilidades;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace LaCafeteria.Pages
 {
     public class RevisarArticuloModel : PageModel
     {
-
         [BindProperty]
         public int opinion { get; set; }
-
+        
         [BindProperty]
         public int contribucion { get; set; }
-
+        
         [BindProperty]
         public int forma { get; set; }
-
+        
         [BindProperty]
         public int recomendacion { get; set; }
 
         [BindProperty]
-        public int comentario { get; set; }
+        public string comentario { get; set; }
 
-        public void OnGet()
+        public RevisarArticuloModel()
         {
-
+            forma = -1;
+            opinion = -1;
+            contribucion = -1;
+            recomendacion = -1;
+            comentario = "";
         }
 
-        public void OnPost()
+        public IActionResult OnPostEnviar()
         {
+            if (opinion < 0 || contribucion < 0 || forma < 0 || recomendacion < 0 || comentario == "")
+            {
+                Notificaciones.Set(this, "revision", "Debe realizar todas las calificaciones", Notificaciones.TipoNotificacion.Error);
+                return Page();
+            }
 
+            int calif_opinion = opinion;
+            int calif_forma = forma;
+            int calif_contribucion = contribucion;
+
+            /* Crear nuevo controlador de revisor de artículo */
+
+
+
+            return Page();
         }
     }
 }
