f677fcb07d31c24481b5fca20d432a4375dcb90a
KewQiu <7310376+KewQiu@users.noreply.github.com>
2020-06-27 02:35:55 -0600

    [Refactoring] Se crea clase InformacionCategoriaTopicoController con metodo GetCategoriasYTopicos

---
+++ LaCafeteria/LaCafeteria/Controllers/InformacionCategoriaTopicoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LaCafeteria.Models;
+using LaCafeteria.Models.Handlers;
+
+namespace LaCafeteria.Controllers
+{
+    public class InformacionCategoriaTopicoController
+    {
+        private InformacionCategoriaTopicosDBHandler informacionCategoriaTopicosDBHandler;
+
+        public InformacionCategoriaTopicoController() {
+            informacionCategoriaTopicosDBHandler = new InformacionCategoriaTopicosDBHandler();
+        }
+
+        public List<CategoriaTopicoModel> GetCategoriasYTopicos() {
+            return informacionCategoriaTopicosDBHandler.GetCategoriasYTopicos();
+        }
+    }
+}
