19f3994272ecb6605ffc11e74562886d37dc1c13
Marco Mora <marco8mora@gmail.com>
2020-06-27 03:28:54 -0600

    [VerReporteFiltrable] Se agrega la clase DatosGraficoModel para representar los datos que grafican los reportes.

---
+++ LaCafeteria/LaCafeteria/Models/DatosGraficoModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaCafeteria.Models
+{
+    public class DatosGraficoDona
+    {
+        public string categoria { get; set; }
+
+        public int cantidad { get; set; }
+
+        public DatosGraficoDona(string cat, int cant)
+        {
+            categoria = cat;
+            cantidad = cant;
+        }
+    }
+
+    public class DatosGraficoBarrasApilado
+    {
+        public string nombreCategoria { get; set; }
+
+        public string nombreSubcategoria { get; set; }
+
+        public int cantidad { get; set; }
+
+        public DatosGraficoBarrasApilado(string nomCat, string nomSub, int cant)
+        {
+            nombreCategoria = nomCat;
+            nombreSubcategoria = nomSub;
+            cantidad = cant;
+        }
+    }
+}
