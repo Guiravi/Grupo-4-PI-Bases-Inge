48b34af64f355519778172ff09c9e04210b5ddbd
KewQiu <7310376+KewQiu@users.noreply.github.com>
2020-06-27 01:12:48 -0600

    [Refactoring] Correccion de typo en InformacionMiembroDBHandler

--- LaCafeteria/LaCafeteria/Models/Handlers/InformacionMiembroDBHandler.cs
+++ LaCafeteria/LaCafeteria/Models/Handlers/InformacionMiembroDBHandler.cs
@@ -5,7 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LaCafeteria.Models.Handlers;
+namespace LaCafeteria.Models.Handlers
 {
 	public class InformacionMiembroDBHandler
 	{
