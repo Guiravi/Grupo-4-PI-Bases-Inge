3130c3dcc32956979612c8eb5ea92f9b2fc0f85d
Guillermo Antonio Ramírez Villalta <guillermo.ramirezvillalta@outlook.com>
2020-06-28 00:52:16 -0600

    [Enviar opinion] Se hace crea la vista de mis artículos

--- LaCafeteria/LaCafeteria/Controllers/BuscadorArticuloController.cs
+++ LaCafeteria/LaCafeteria/Controllers/BuscadorArticuloController.cs
@@ -46,6 +46,12 @@ namespace LaCafeteria.Controllers
             return artList;
         }
 
+        public List<ArticuloModel> GetArticulosPorEstadoAsignaMiem(string estadoArticulo, string username)
+        {
+            List<ArticuloModel> artList = buscadorArticuloDBHandler.GetArticulosPorEstadoAsignaMiem(estadoArticulo, username);
+            return artList;
+        }
+
 
         public List<ArticuloModel> GetArticulosRevisionFinalizada() {
             return buscadorArticuloDBHandler.GetArticulosRevisionFinalizada();
---
+++ LaCafeteria/LaCafeteria/Pages/MisArticulosPorRevisar.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using LaCafeteria.Controllers;
+using LaCafeteria.Models;
+
+namespace LaCafeteria.Pages
+{
+    public class MisArticulosPorRevisarModel : PageModel
+    {
+        private BuscadorArticuloController buscadorArticuloController { get; set; }
+        private InformacionArticuloController informacionArticuloController { get; set; }
+
+        public List<ArticuloModel> artList { get; set; }
+
+        public Dictionary<ArticuloModel, List<CategoriaTopicoModel>> dictTopicos { get; set; }
+
+        public Dictionary<ArticuloModel, string> dictAutores { get; set; } 
+
+        public MisArticulosPorRevisarModel() {
+
+            buscadorArticuloController = new BuscadorArticuloController();
+            informacionArticuloController = new InformacionArticuloController();
+
+            artList = buscadorArticuloController.GetArticulosPorEstadoAsignaMiem(EstadoArticulo.RequiereRevision, Request.Cookies["usernamePK"]);
+
+            dictTopicos = new Dictionary<ArticuloModel, List<CategoriaTopicoModel>>();
+            dictAutores = new Dictionary<ArticuloModel, string>();
+
+            for (int i = 0; i<artList.Count(); ++i )
+            {
+                dictTopicos.Add(artList[i], informacionArticuloController.GetCategoriaTopicosArticulo(artList[i].articuloAID));
+                dictAutores.Add(artList[i], informacionArticuloController.GetAutoresDeArticuloString(artList[i].articuloAID));
+            }
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
