ad5089c55d7f73bbc167dc47db1dabb6fbe073c4
juan herrera <jjherrerarodriguez98@gmail.com>
2020-06-28 01:29:21 -0600

    Integrar el código de lógica con la interfaz de usuario

---
+++ LaCafeteria/LaCafeteria/Pages/PromoverMiembro.cshtml.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using LaCafeteria.Models;
+using LaCafeteria.Controllers;
+using LaCafeteria.Utilidades;
+using LaCafeteria.Models;
+namespace LaCafeteria.Pages
+{
+    public class PromoverMiembroModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public string usernamePK { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string nombreRolFK { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int aceptar { get; set; }
+        public string usernameMiembroFK { get; set; }
+        public string rolNucleoFK { get; set; }
+        public List<MiembroModel> miembros { get; set; }
+        public EditorMiembroController miembroController;
+        public BuscadorMiembrosController buscadorMiembroController;
+        public  EditorMiembroSolicitaSubirRangoNucleoController editorMiembroSolicitaSubirRangoNucleoController;
+        public RevisionSolicitudesPreviasMiembroSubirRangoNucleoController revisionSolicitudesPreviasMiembroSubirRangoNucleoController;
+        public CreadorNotificacionController creadorNotificacionController;
+        public PromoverMiembroModel()
+        {
+
+            editorMiembroSolicitaSubirRangoNucleoController = new EditorMiembroSolicitaSubirRangoNucleoController();
+            revisionSolicitudesPreviasMiembroSubirRangoNucleoController = new RevisionSolicitudesPreviasMiembroSubirRangoNucleoController();
+            buscadorMiembroController = new BuscadorMiembrosController();
+            creadorNotificacionController = new CreadorNotificacionController();
+            miembroController = new EditorMiembroController();
+        }
+
+        public void OnGet()
+        {
+            usernameMiembroFK = Request.Cookies["usernamePK"];
+            if (nombreRolFK != "nulo") {
+                rolNucleoFK = buscadorMiembroController.GetRango(usernameMiembroFK);
+                if (aceptar == 1)
+                {
+                    VotarPromover();
+                }
+                else {
+                    VotarRechazar();
+                }
+
+            }
+            miembros = buscadorMiembroController.GetListaMiembrosSolicitud(usernameMiembroFK);
+        }
+
+        public void VotarPromover()
+        {
+
+            if (nombreRolFK != "Periférico" && nombreRolFK != "Activo" )
+            {
+                Notificaciones.Set(this, "rangoInvalido", "El rango de este miembro no califica para poder pormoverse", Notificaciones.TipoNotificacion.Error);
+            }
+            else {
+                if (nombreRolFK == "Núcleo")
+                {
+                    Notificaciones.Set(this, "rangoPeriferico", "El rango de este miembro no se puede votar para aumentar", Notificaciones.TipoNotificacion.Error);
+                }
+                 else
+                {
+                    editorMiembroSolicitaSubirRangoNucleoController.VotarPromover(usernamePK,usernameMiembroFK);
+                    Notificaciones.Set(this, "exitoVotar", "Su voto ha sido emitido", Notificaciones.TipoNotificacion.Exito);
+                    revisarVotosAceptacion();
+                }
+            }
+
+
+
+           
+        }
+
+        public void VotarRechazar()
+        {
+
+            if (nombreRolFK != "Periférico" && nombreRolFK != "Activo")
+            {
+                Notificaciones.Set(this, "rangoInvalido", "El rango de este miembro no califica para poder pormoverse", Notificaciones.TipoNotificacion.Error);
+            }
+            else
+            {
+                if (nombreRolFK == "Núcleo")
+                {
+                    Notificaciones.Set(this, "rangoPeriferico", "El rango de este miembro no se puede votar para aumentar", Notificaciones.TipoNotificacion.Error);
+                }
+                else
+                {
+                    editorMiembroSolicitaSubirRangoNucleoController.VotarRechazar(usernamePK, usernameMiembroFK);
+                    Notificaciones.Set(this, "exitoVotar", "Su voto ha sido emitido", Notificaciones.TipoNotificacion.Exito);
+                    revisarVotosRechazo();
+                }
+            }
+
+
+
+
+        }
+        public void revisarVotosAceptacion() {
+            int votosTotales = revisionSolicitudesPreviasMiembroSubirRangoNucleoController.VerSiSolicitado(usernamePK);
+            int votosAceptados = revisionSolicitudesPreviasMiembroSubirRangoNucleoController.VerTodosSolicitadosAceptados(usernamePK);
+            double porcentajeAceptacion = ((double)votosAceptados / (double)votosTotales) * (double)100;
+            if (porcentajeAceptacion > 50 || (nombreRolFK == "Activo" && rolNucleoFK == "Coordinador"))
+            {
+                miembroController.AscenderMiembro(usernamePK, nombreRolFK);
+                editorMiembroSolicitaSubirRangoNucleoController.BorrarSolicitudes(usernamePK);
+                string mensaje = "Su rango ha sido promovido satisfactoriamente";
+                Notificacion notificacion = new Notificacion(usernamePK, mensaje, null);
+                creadorNotificacionController.CrearNotificacion(notificacion);
+                /*Notid=ficacion Aceptacion*/
+            }
+            else {
+                int votosRechazados = revisionSolicitudesPreviasMiembroSubirRangoNucleoController.VerTodosSolicitadosRechazados(usernamePK);
+                double porcentajeRechazados = ((double)votosRechazados / (double)votosTotales) * (double)100;
+                if (porcentajeRechazados == 50 && porcentajeAceptacion == 50) {
+                    string mensaje = "Su rango no pudo ser promovdio dado un empate de votos";
+                    Notificacion notificacion = new Notificacion(usernamePK, mensaje, null);
+                    creadorNotificacionController.CrearNotificacion(notificacion);
+                }
+            }
+        }
+
+        public void revisarVotosRechazo()
+        {
+            int votosTotales = revisionSolicitudesPreviasMiembroSubirRangoNucleoController.VerSiSolicitado(usernamePK);
+            int votosRechazados = revisionSolicitudesPreviasMiembroSubirRangoNucleoController.VerTodosSolicitadosRechazados(usernamePK);
+            double porcentajeRechazados = ((double)votosRechazados / (double)votosTotales) * (double)100;
+            if (porcentajeRechazados > 50 || (nombreRolFK == "Activo" && rolNucleoFK == "Coordinador"))
+            {
+
+                editorMiembroSolicitaSubirRangoNucleoController.BorrarSolicitudes(usernamePK);
+                string mensaje = "Su rango ha sido promovido ya que no tuvo la aprobación mínima";
+                Notificacion notificacion = new Notificacion(usernamePK, mensaje, null);
+                creadorNotificacionController.CrearNotificacion(notificacion);
+                /*Notificacion Rechazo*/
+            }
+            else
+            {
+                int votosAceptados = revisionSolicitudesPreviasMiembroSubirRangoNucleoController.VerTodosSolicitadosAceptados(usernamePK);
+                double porcentajeAceptacion = ((double)votosAceptados / (double)votosTotales) * (double)100;
+                if (porcentajeRechazados == 50 && porcentajeAceptacion == 50)
+                {
+                    string mensaje = "Su rango no pudo ser promovdio dado un empate de votos";
+                    Notificacion notificacion = new Notificacion(usernamePK, mensaje, null);
+                    creadorNotificacionController.CrearNotificacion(notificacion);
+                }
+            }
+        }
+    }
+
+}
---
+++ LaCafeteria/LaCafeteria/Pages/SolicitudSubirRango.cshtml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using LaCafeteria.Controllers;
+using LaCafeteria.Utilidades;
+using LaCafeteria.Models;
+namespace LaCafeteria.Pages
+{
+    public class SolicitudSubirRangoModel : PageModel
+    {
+        string usernamePK;
+        string nombreRolFK;
+        public BuscadorMiembrosController buscadorMiembrosController;
+        public EditorMiembroSolicitaSubirRangoNucleoController miembroSolicitaSubirRangoNucleoEnviadaController;
+        public RevisionSolicitudesPreviasMiembroSubirRangoNucleoController revisionSolicitudesPreviasMiembroSubirRangoNucleoController;
+        public CreadorNotificacionController creadorNotificacionController;
+        public List<MiembroModel> miembros { get; set; }
+        public SolicitudSubirRangoModel()
+        {
+
+            buscadorMiembrosController = new BuscadorMiembrosController();
+            miembroSolicitaSubirRangoNucleoEnviadaController = new EditorMiembroSolicitaSubirRangoNucleoController();
+            revisionSolicitudesPreviasMiembroSubirRangoNucleoController = new RevisionSolicitudesPreviasMiembroSubirRangoNucleoController();
+            creadorNotificacionController = new CreadorNotificacionController();
+        }
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPostSolicitar()
+        {
+            if (Request.Cookies["usernamePK"] != null)
+            {
+                miembros = buscadorMiembrosController.GetListaNucleosSolicitud();
+                usernamePK = Request.Cookies["usernamePK"];
+                nombreRolFK = buscadorMiembrosController.GetRango(usernamePK);
+                if (nombreRolFK != "Periférico" && nombreRolFK != "Activo" )
+                {
+                    Notificaciones.Set(this, "rangoInvalido", "El rango de este miembro no califica para la solicitud", Notificaciones.TipoNotificacion.Error);
+                }
+                else
+                {
+                    int puede = revisionSolicitudesPreviasMiembroSubirRangoNucleoController.VerSiSolicitado(usernamePK);
+
+                    if (puede == 0)
+                    {
+                        miembroSolicitaSubirRangoNucleoEnviadaController.SolicitarSubirRango(usernamePK, miembros);
+                        Notificaciones.Set(this, "exitoSolicitud", "La solicitud se envió con éxito", Notificaciones.TipoNotificacion.Exito);
+                        foreach (var miembro in miembros) {
+                            string mensaje = "Hay que revisar la solicitud para subir de rango del miembro "+ usernamePK;
+                            Notificacion notificacion = new Notificacion(miembro.usernamePK, mensaje, null);
+                            creadorNotificacionController.CrearNotificacion(notificacion);
+                        }
+                    }
+                    else
+                    {
+                        Notificaciones.Set(this, "fracasoSolicitud", "Usted ha enviado una solicitud que sigue en valoración", Notificaciones.TipoNotificacion.Error);
+                    }
+                }
+            }
+                
+
+            return Page();
+        }
+
+    }
+}
