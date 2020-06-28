df33307688522f8a5ff088d049dd3c3c245ba4d3
juan herrera <jjherrerarodriguez98@gmail.com>
2020-06-28 01:34:14 -0600

    Integrar las consultas al código de lógica para la historia de solicitar subir de rango en la comunidad

---
+++ LaCafeteria/LaCafeteria/Controllers/RevisionSolicitudesPreviasMiembroSubirRangoNucleoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LaCafeteria.Models;
+
+namespace LaCafeteria.Controllers
+{
+    public class RevisionSolicitudesPreviasMiembroSubirRangoNucleoController
+    {
+        public RevisionSolicitudesPreviasMiembroSubirRangoNucleoDBHandler revisionSolicitudesPreviasMiembroSubirRangoNucleoDBHandler;
+
+        public RevisionSolicitudesPreviasMiembroSubirRangoNucleoController()
+        {
+            revisionSolicitudesPreviasMiembroSubirRangoNucleoDBHandler = new RevisionSolicitudesPreviasMiembroSubirRangoNucleoDBHandler();
+        }
+        public int VerSiSolicitado(string usernamePK)
+        {
+            return revisionSolicitudesPreviasMiembroSubirRangoNucleoDBHandler.VerSiSolicitado(usernamePK);
+        }
+        public int VerTodosSolicitadosAceptados(string usernamePK) {
+            return revisionSolicitudesPreviasMiembroSubirRangoNucleoDBHandler.VerTodosSolicitadosAceptados(usernamePK);
+        }
+        public int VerTodosSolicitadosRechazados(string usernamePK)
+        {
+            return revisionSolicitudesPreviasMiembroSubirRangoNucleoDBHandler.VerTodosSolicitadosRechazados(usernamePK);
+        }
+    }
+    
+}
