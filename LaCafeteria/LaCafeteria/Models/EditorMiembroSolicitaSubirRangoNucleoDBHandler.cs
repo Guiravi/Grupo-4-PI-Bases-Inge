ec94ff0b85f4bf86419bd4b4be981c3fee3a672a
juan herrera <jjherrerarodriguez98@gmail.com>
2020-06-28 01:31:37 -0600

    Generar las consultas necesarias a la base de datos para la historia de solicitar subir de rango en la comunidad

---
+++ LaCafeteria/LaCafeteria/Models/EditorMiembroSolicitaSubirRangoNucleoDBHandler.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using LaCafeteria.Utilidades;
+
+namespace LaCafeteria.Models
+{
+    public class EditorMiembroSolicitaSubirRangoNucleoDBHandler
+    {
+
+
+        public void VotarPromover(string usernamePK, string usernameMiembroFK)
+        {
+            int solicitudes = 0;
+            string connectionString = AppSettings.GetConnectionString();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+
+                string sqlString = @"UPDATE dbo.[MiembroSolicitaSubirRangoNucleo] SET estado =  'Aceptado' WHERE usernameMiembroFK = @usernamePK AND usernameNucleoFK = @usernameNucleoFK AND estado IS NULL";
+
+                sqlConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(sqlString, sqlConnection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@usernamePK", usernamePK);
+                    sqlCommand.Parameters.AddWithValue("@usernameNucleoFK", usernameMiembroFK);
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
+
+
+        }
+        public void VotarRechazar(string usernamePK, string usernameMiembroFK)
+        {
+            int solicitudes = 0;
+            string connectionString = AppSettings.GetConnectionString();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+
+                string sqlString = @"UPDATE dbo.[MiembroSolicitaSubirRangoNucleo] SET estado =  'Rechazado' WHERE usernameMiembroFK = @usernamePK AND usernameNucleoFK = @usernameNucleoFK AND estado IS NULL" ;
+
+                sqlConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(sqlString, sqlConnection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@usernamePK", usernamePK);
+                    sqlCommand.Parameters.AddWithValue("@usernameNucleoFK", usernameMiembroFK);
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
+
+
+        }
+
+        public void BorrarSolicitudes(string usernamePK)
+        {
+            int solicitudes = 0;
+            string connectionString = AppSettings.GetConnectionString();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+
+                string sqlString = @"DELETE FROM dbo.[MiembroSolicitaSubirRangoNucleo]  WHERE usernameMiembroFK = @usernamePK  ";
+
+                sqlConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(sqlString, sqlConnection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@usernamePK", usernamePK);
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
+
+
+        }
+
+        public void SolicitarSubirRango(string usernamePK, List<MiembroModel> miembros)
+        {
+            string connectionString = AppSettings.GetConnectionString();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {      
+
+                string sqlString = @"INSERT INTO dbo.[MiembroSolicitaSubirRangoNucleo](usernameNucleoFK, usernameMiembroFK, estado)
+											 
+
+									VALUES(@usernameNucleoFK,@usernamePK , null)";
+
+                sqlConnection.Open();
+                foreach (var miembro in miembros) {
+                    using (SqlCommand sqlCommand = new SqlCommand(sqlString, sqlConnection))
+                    {
+                        sqlCommand.Parameters.AddWithValue("@usernamePK", usernamePK);
+                        sqlCommand.Parameters.AddWithValue("@usernameNucleoFK", miembro.usernamePK);
+                        
+                       
+
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+    }
+}	
+
