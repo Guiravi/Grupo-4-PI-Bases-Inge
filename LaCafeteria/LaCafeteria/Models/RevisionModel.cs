471674db03151a737aaa36ca705e387af6065d2c
KewQiu <7310376+KewQiu@users.noreply.github.com>
2020-06-28 01:59:24 -0600

    [Refactoring] Se crea un modelo de Revisiones para facilitar el acceso a la revision y actualizacion de articulos

---
+++ LaCafeteria/LaCafeteria/Models/RevisionModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaCafeteria.Models
+{
+    public class RevisionModel
+    {
+        public string usernameMiemFK { get; set; }
+        public int idArticuolFK { get; set; }
+        public string estadoRevision { get; set; }
+        public double puntaje { get; set; }
+        public int opinion { get; set; }
+        public int contribucion { get; set; }
+        public int forma { get; set; }
+        public string recomendacion { get; set; }
+        public string comentarios { get; set; }
+    }
+}
