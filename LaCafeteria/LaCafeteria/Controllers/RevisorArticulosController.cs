124907514d1e0cbe19d94879ef5847151ab74568
KewQiu <7310376+KewQiu@users.noreply.github.com>
2020-06-27 23:54:06 -0600

    [FormularioRevsion] Se crea metodo NotificarCoordinadorRevisionFinalizada en la clase RevisarArticulosController

--- LaCafeteria/LaCafeteria/Controllers/RevisorArticulosController.cs
+++ LaCafeteria/LaCafeteria/Controllers/RevisorArticulosController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using LaCafeteria.Models;
 using LaCafeteria.Models.Handlers;
 
 namespace LaCafeteria.Controllers
@@ -9,10 +10,16 @@ namespace LaCafeteria.Controllers
     public class RevisorArticulosController
     {
         private RevisorArticuloHandler revisorArticuloHandler;
+        private CreadorNotificacionController creadorNotificacionController;
+        private BuscadorMiembrosController buscadorMiembrosController;
+        private InformacionArticuloController informacionArticuloController;
         
         public RevisorArticulosController()
         {
-
+            revisorArticuloHandler = new RevisorArticuloHandler();
+            creadorNotificacionController = new CreadorNotificacionController();
+            buscadorMiembrosController = new BuscadorMiembrosController();
+            informacionArticuloController = new InformacionArticuloController();
         }
 
         public void ActualizarRevisionArticulo(double merito, int opinion, int contribucion, int forma, string estadoRevision,
@@ -20,6 +27,20 @@ namespace LaCafeteria.Controllers
         {
             revisorArticuloHandler.ActualizarEstadoRevisionArticulo(merito, opinion, contribucion, forma, estadoRevision,
                                                     comentarios, recomendacion, username, idArticulo);
+
+            NotificarCoordinadorRevisionFinalizada(idArticulo);
         }
+
+        private void NotificarCoordinadorRevisionFinalizada(int id) {
+            if ( revisorArticuloHandler.ArticuloConRevisionFinalizada(id) )
+            {
+                MiembroModel coordinador = buscadorMiembrosController.GetMiembroCoordinador();
+                ArticuloModel articulo = informacionArticuloController.GetInformacionArticuloModel(id);
+                string mensaje = "El artículo " + articulo.titulo + " ha sido revisado por todos sus revisores. Por favor tomar una decisión.";
+                Notificacion notificacion = new Notificacion(coordinador.usernamePK, mensaje, "/ArticulosRevisados");
+                creadorNotificacionController.CrearNotificacion(notificacion);
+            }
+        }
+
     }
 }
