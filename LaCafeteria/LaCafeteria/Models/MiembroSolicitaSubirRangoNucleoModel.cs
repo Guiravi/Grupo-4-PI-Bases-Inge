db37b7652c66e57b7739bcad0d0ede79008e95be
juan herrera <jjherrerarodriguez98@gmail.com>
2020-06-28 01:43:30 -0600

    Crear modelo para manupilar información a ,nivel de base de datos, relacioanada a cuando el miembro solicita subir de rango

---
+++ LaCafeteria/LaCafeteria/Models/MiembroSolicitaSubirRangoNucleoModel.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaCafeteria.Models
+{
+    public class MiembroSolicitaSubirRangoNucleoModel
+    {
+        public string usernameNucleoFK { get; set; }
+
+        public string usernameMiembroFK { get; set; }
+
+        public string estado { get; set; }
+    }
+}
