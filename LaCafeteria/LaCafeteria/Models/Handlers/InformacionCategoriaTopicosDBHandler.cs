ee35d1ac4e7ab8adb192b7a51ef94a8c193c40ee
Marco Mora <marco8mora@gmail.com>
2020-06-28 03:50:26 -0600

    [VerReporteFiltrable] Se crea la función GetCategoriasTopicosNoAsociadosRol(string rol) que devuelve una lista de las categorias y tópicos que no tienen artículos asociados a un rol.

--- LaCafeteria/LaCafeteria/Models/Handlers/InformacionCategoriaTopicosDBHandler.cs
+++ LaCafeteria/LaCafeteria/Models/Handlers/InformacionCategoriaTopicosDBHandler.cs
@@ -40,5 +40,92 @@ namespace LaCafeteria.Models.Handlers
             }
             return lista;
         }
+
+        public List<CategoriaTopicoModel> GetCategoriasTopicosNoAsociadosRol(string rol)
+        {
+            List<CategoriaTopicoModel> lista = new List<CategoriaTopicoModel>();
+
+            if (rol == "") //Todos lo roles
+            {
+                string connectionString = AppSettings.GetConnectionString();
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+
+                    string sqlString = @"SELECT CT.nombreCategoriaPK, CT.nombreTopicoPK
+                                        FROM [dbo].[CategoriaTopico] CT
+                                        WHERE CT.nombreCategoriaPK + ' ' + CT.nombreTopicoPK NOT IN (SELECT ATT.nombreCategoriaFK + ' ' + ATT.nombreTopicoFK
+															                                        FROM [dbo].[ArticuloTrataTopico] ATT
+															                                        JOIN [dbo].[Articulo] A
+																                                        ON ATT.idArticuloFK = A.articuloAID
+															                                        JOIN [dbo].[MiembroAutorDeArticulo] MAA
+																                                        ON A.articuloAID = MAA.idArticuloFK
+															                                        JOIN [dbo].[Miembro] M
+																                                        ON MAA.usernameMiemFK = M.usernamePK
+															                                        WHERE A.estado = 'Publicado')";
+
+                    sqlConnection.Open();
+                    using (SqlCommand sqlCommand = new SqlCommand(sqlString, sqlConnection))
+                    {
+                        using (SqlDataReader dataReader = sqlCommand.ExecuteReader())
+                        {
+                            while (dataReader.Read())
+                            {
+                                CategoriaTopicoModel categoriaTopico = new CategoriaTopicoModel()
+                                {
+                                    nombreCategoriaPK = (string)dataReader["nombreCategoriaPK"],
+                                    nombreTopicoPK = (string)dataReader["nombreTopicoPK"]
+                                };
+
+                                lista.Add(categoriaTopico);
+                            }
+
+                        }
+                    }
+                }
+            }
+            else
+            {
+                string connectionString = AppSettings.GetConnectionString();
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+
+                    string sqlString = @"SELECT CT.nombreCategoriaPK, CT.nombreTopicoPK
+                                        FROM [dbo].[CategoriaTopico] CT
+                                        WHERE CT.nombreCategoriaPK + ' ' + CT.nombreTopicoPK NOT IN (SELECT ATT.nombreCategoriaFK + ' ' + ATT.nombreTopicoFK
+															                                        FROM [dbo].[ArticuloTrataTopico] ATT
+															                                        JOIN [dbo].[Articulo] A
+																                                        ON ATT.idArticuloFK = A.articuloAID
+															                                        JOIN [dbo].[MiembroAutorDeArticulo] MAA
+																                                        ON A.articuloAID = MAA.idArticuloFK
+															                                        JOIN [dbo].[Miembro] M
+																                                        ON MAA.usernameMiemFK = M.usernamePK
+															                                        WHERE A.estado = 'Publicado'
+																                                        AND M.nombreRolFK = @rol)";
+
+                    sqlConnection.Open();
+                    using (SqlCommand sqlCommand = new SqlCommand(sqlString, sqlConnection))
+                    {
+                        sqlCommand.Parameters.AddWithValue("@rol", rol);
+                        using (SqlDataReader dataReader = sqlCommand.ExecuteReader())
+                        {
+                            while (dataReader.Read())
+                            {
+                                CategoriaTopicoModel categoriaTopico = new CategoriaTopicoModel()
+                                {
+                                    nombreCategoriaPK = (string)dataReader["nombreCategoriaPK"],
+                                    nombreTopicoPK = (string)dataReader["nombreTopicoPK"]
+                                };
+
+                                lista.Add(categoriaTopico);
+                            }
+
+                        }
+
+                    }                    
+                }
+            }
+
+            return lista;
+        }
     }
 }
