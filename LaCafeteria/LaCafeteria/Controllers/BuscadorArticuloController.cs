04c096b563da2232565ed5394251ec899d0695ad
Marco Mora <marco8mora@gmail.com>
2020-06-29 19:43:10 -0600

    [Refactoring] Correción en nombres de funciones del BuscadorArticuloDBHandler al ser llamados por el controller

--- LaCafeteria/LaCafeteria/Controllers/BuscadorArticuloController.cs
+++ LaCafeteria/LaCafeteria/Controllers/BuscadorArticuloController.cs
@@ -32,14 +32,14 @@ namespace LaCafeteria.Controllers
                         nombreTopicoPK = separacion[1]
                     };
 
-                    articulos.AddRange(buscadorArticuloDBHandler.GetArticulosPorTopico(modelo, solicitud.tiposArticulo));
+                    articulos.AddRange(buscadorArticuloDBHandler.GetArticulosPorTopicoYTipo(modelo, solicitud.tiposArticulo));
                 }
 
                 articulos = articulos.Distinct(new ItemEqualityComparer()).ToList();
 
             } else if ( solicitud.tipoBusqueda == "titulos" )
             {
-                articulos = buscadorArticuloDBHandler.GetArticulosPorTitulo(solicitud.textoBusqueda, solicitud.tiposArticulo);
+                articulos = buscadorArticuloDBHandler.GetArticulosPorTituloYTipo(solicitud.textoBusqueda, solicitud.tiposArticulo);
             } else
             {
                 articulos = buscadorArticuloDBHandler.GetArticulosPorAutorYTipo(solicitud.textoBusqueda, solicitud.tiposArticulo).Distinct(new ItemEqualityComparer()).ToList();
