3e5e0901cf7bb32f477ab82d9c1e313eb34b89bc
Silvio Salazar <silviosalac@gmail.com>
2020-07-01 00:06:42 -0600

    [InteresaRevisar] Se despliega el contenido del articulo para cualquiera de los 3 tipos en la vista InteresaRevisar.

---
+++ LaCafeteria/LaCafeteria/Pages/InteresaRevisar.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LaCafeteria.Controllers;
+using LaCafeteria.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LaCafeteria.Pages
+{
+	public class InteresaRevisarModel : PageModel
+	{
+		private InformacionArticuloController informacionArticuloController;
+		private DocumentosArticuloController documentosArticuloController;
+
+		[BindProperty(SupportsGet = true)]
+		public int articuloAID { get; set; }
+
+		public ArticuloModel articulo { get; set; }
+		public string rutaCarpeta { get; set; }
+
+		public InteresaRevisarModel(IHostingEnvironment env)
+		{
+			informacionArticuloController = new InformacionArticuloController();
+			rutaCarpeta = env.WebRootPath;
+		}
+
+		public void OnGet()
+		{
+			articulo = informacionArticuloController.GetInformacionArticuloModel(articuloAID);
+
+			if (articulo.tipo == "Largo")
+			{
+				documentosArticuloController = new DocumentosArticuloController();
+				documentosArticuloController.CargarArticuloPDF(articuloAID, rutaCarpeta);
+			}
+		}
+
+		public ActionResult OnPostMeInteresaRevisar()
+		{
+
+			return Redirect("InteresaRevisar");
+		}
+	}
+}
