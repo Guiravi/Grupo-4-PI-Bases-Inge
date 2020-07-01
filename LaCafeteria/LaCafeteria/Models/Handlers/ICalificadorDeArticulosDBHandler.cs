5fa2e71c99357545ce98a0a2cd878ec8581b39ed
KewQiu <7310376+KewQiu@users.noreply.github.com>
2020-07-01 05:21:03 -0600

    [UnitTesting] Se crean pruebas para calificar articulos y se aplican correcciones para pasar las pruebas

---
+++ LaCafeteria/LaCafeteria/Models/Handlers/ICalificadorDeArticulosDBHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace LaCafeteria.Models.Handlers
+{
+    public interface ICalificadorDeArticulosDBHandler
+    {
+        void CalificarArticulo(string username, int idArticulo, int valorCalif);
+    }
+}
